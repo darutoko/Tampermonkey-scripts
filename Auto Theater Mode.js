2adcbeaf5e4acda4d9d3f687d50871131c3c146c
darutoko <darutoko@yandex.ru>
2017-12-17 19:14:09 +0700

    move expand player logic on button click event

--- Auto Theater Mode.js
+++ Auto Theater Mode.js
@@ -17,21 +17,22 @@
 
             if(!button) return;
 
+            button.addEventListener('click', function(){
+                setTimeout(function() {
+                    var div;
+                    // Hide whispers button
+                    div = document.getElementsByClassName('whispers--theatre-mode')[0];
+                    console.log('.whispers--theatre-mode: ' + div);
+                    if(div) div.style.display = 'none';
+                    // Expand player to fill space bottom
+                    div = document.getElementsByClassName('video-player--logged-in')[0];
+                    console.log('.video-player--logged-in: ' + div);
+                    if(div) div.classList.remove('video-player--logged-in');
+                }, 500);
+            });
+
             var event = new MouseEvent('click', {'bubbles': true});
             button.dispatchEvent(event);
         }, 4000);
-    });
-    window.addEventListener('load', function() {
-        setTimeout(function() {
-            var div;
-            // Hide whispers button
-            div = document.getElementsByClassName('whispers--theatre-mode')[0];
-            console.log('.whispers--theatre-mode: ' + div);
-            if(div) div.style.display = 'none';
-            // Expand player to fill space
-            div = document.getElementsByClassName('video-player--logged-in')[0];
-            console.log('.video-player--logged-in: ' + div);
-            if(div) div.classList.remove('video-player--logged-in');
-        }, 6000);
     });
 })();
