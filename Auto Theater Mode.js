03c02e5c35a7c218cff6b3fc7dd5dbeea0f50368
darutoko <darutoko@yandex.ru>
2017-12-17 19:02:15 +0700

    add player expand for when logged in and refactor

--- Auto Theater Mode.js
+++ Auto Theater Mode.js
@@ -10,24 +10,28 @@
 
 (function () {
     'use strict';
-    window.onload = function() {
-        setTimeout(function () {
-            var div = document.getElementById('js-controls-bottom');
-            console.log( '#js-controls-bottom: ' + div );
-            if(div === null) return;
+    window.addEventListener('load', function() {
+        setTimeout(function() {
+            var button = document.getElementsByClassName('qa-theatre-mode-button')[0];
+            console.log( '.qa-theatre-mode-button: ' + button );
 
-            var button;// = div.childNodes[0].childNodes[1].childNodes[2];
-            var children = div.childNodes[0].childNodes;
-            for(var i=0; i < children.length; i++) {
-                if (children[i].className === "player-buttons-right") {
-                    button = children[i].childNodes[2];
-                    break;
-                }
-            }
-            console.log( 'button: ' + button );
-
+            if(!button) return;
+            
             var event = new MouseEvent('click', {'bubbles': true});
             button.dispatchEvent(event);
+        }, 4000);
+    });
+    window.addEventListener('load', function() {
+        setTimeout(function() {
+            var div;
+            // Hide whispers button
+            div = document.getElementsByClassName('whispers--theatre-mode')[0];
+            console.log('.whispers--theatre-mode: ' + div);
+            div.style.display = 'none';
+            // Expand player to fill space
+            div = document.getElementsByClassName('video-player--logged-in')[0];
+            console.log('.video-player--logged-in: ' + div);
+            div.classList.remove('video-player--logged-in');
         }, 6000);
-    };
+    });
 })();
