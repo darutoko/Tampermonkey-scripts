05e39f9aad192dec1638473ef6d2585b30213e8d
darutoko <darutoko@yandex.ru>
2018-03-23 19:32:52 +0700

    move expand player logic to body click event and hide whispers logic to one time run function

--- Auto Theater Mode.js
+++ Auto Theater Mode.js
@@ -17,15 +17,11 @@
 
             if(!button) return;
 
-            button.addEventListener('click', function(){
+            document.body.addEventListener('click', function(){
                 setTimeout(function() {
                     var div;
-                    // Hide whispers button
-                    div = document.getElementsByClassName('whispers--theatre-mode')[0];
-                    console.log('.whispers--theatre-mode: ' + div);
-                    if(div) div.style.display = 'none';
                     // Expand player to fill space bottom
-                    div = document.getElementsByClassName('video-player--logged-in')[0];
+                    div = document.getElementsByClassName('video-player--theatre video-player--logged-in')[0];
                     console.log('.video-player--logged-in: ' + div);
                     if(div) div.classList.remove('video-player--logged-in');
                 }, 500);
@@ -33,6 +29,14 @@
 
             var event = new MouseEvent('click', {'bubbles': true});
             button.dispatchEvent(event);
+
+            setTimeout(function() {
+                var div;
+                // Hide whispers button
+                div = document.getElementsByClassName('whispers--theatre-mode')[0];
+                console.log('.whispers--theatre-mode: ' + div);
+                if(div) div.style.display = 'none';
+            }, 500);
         }, 4000);
     });
 })();
