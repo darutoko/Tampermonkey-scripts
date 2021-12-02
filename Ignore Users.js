fdb1020b861f21d6e3f77e0eed88a86d417c443e
darutoko <darutoko@gmail.com>
2021-12-02 21:16:52 +0700

    convert indentations to tabs

--- Ignore Users.js
+++ Ignore Users.js
@@ -9,22 +9,22 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    window.addEventListener('load', function() {
+		'use strict';
+		window.addEventListener('load', function() {
 			var users = ['https://glav.su/members/16811/', 'https://glav.su/members/1482/', 'https://glav.su/members/25900/', 'https://glav.su/members/9955/', 'https://glav.su/members/29612/',
-      'https://glav.su/members/8634/', 'https://glav.su/members/2633/', 'https://glav.su/members/35969/', 'https://glav.su/members/25900/', 'https://glav.su/members/11505/'];
-      
-      document.querySelectorAll('.a-mp-m').forEach(message => {
-        // Skip if user is not on the list
-        if (!users.includes( message.querySelector('.a-mp-m-an > a').href )) return;
-        // Hide message
-        let childrenBlocks = message.children;
-        for (let i = 1; i < childrenBlocks.length; i++)
-            childrenBlocks[i].setAttribute('style', 'display: none;');
-        // Downvote message if possible
-        // let button = message.querySelector('.js-ml-m-v-nb');
-        // console.log('.js-ml-m-v-nb: ' + !!button);
-        // if (button) button.dispatchEvent(event);
-      });
-    });
+			'https://glav.su/members/8634/', 'https://glav.su/members/2633/', 'https://glav.su/members/35969/', 'https://glav.su/members/25900/', 'https://glav.su/members/11505/'];
+			
+			document.querySelectorAll('.a-mp-m').forEach(message => {
+				// Skip if user is not on the list
+				if (!users.includes( message.querySelector('.a-mp-m-an > a').href )) return;
+				// Hide message
+				let childrenBlocks = message.children;
+				for (let i = 1; i < childrenBlocks.length; i++)
+						childrenBlocks[i].setAttribute('style', 'display: none;');
+				// Downvote message if possible
+				// let button = message.querySelector('.js-ml-m-v-nb');
+				// console.log('.js-ml-m-v-nb: ' + !!button);
+				// if (button) button.dispatchEvent(event);
+			});
+		});
 })();
